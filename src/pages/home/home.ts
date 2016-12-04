4b33f5b1912fa2461bf696724e35e83f4ca57c08
Marcin Szydelski <marcin@szydelscy.pl>
2016-12-04 02:52:26 +0100

    added sort of phone book

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,19 +2,21 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 import { SmsService } from '../../providers/sms-service';
-
+import { PeopleList } from '../../providers/people-list';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html',
-  providers: [SmsService]
+  providers: [SmsService,PeopleList]
 })
 
 export class HomePage {
   telNo: string;
-  msg: string;
+  public msg: string;
+  public people: any;
 
-  constructor(public navCtrl: NavController, public smsService: SmsService) {
-    
+
+  constructor(public navCtrl: NavController, public smsService: SmsService, public peopleList: PeopleList) {
+     this.loadPeople();
   }
 
 sendSms(){
@@ -25,6 +27,19 @@ sendSms(){
     console.log('not enough data to send sms')
   }
 }
+
+  loadPeople() {
+    this.peopleList.load()
+      .then(data1 => { 
+        this.people = data1;
+      });
+  }    
+  itemSelected(phone,first,last) {
+
+    this.telNo = phone
+    this.msg = "Dear "+first.charAt(0).toUpperCase()+first.slice(1)+" "+last.charAt(0).toUpperCase()+last.slice(1)+". Don't you think it's time to buy new <blablabla>?"
+
+  }
 
 
 }
